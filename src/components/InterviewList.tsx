080a957a05a1d0e994cd6d7bf8407daf9a00f53c
PanachaiStudent <6633136021@student.chula.ac.th>
2024-03-27 00:10:36 +0700

    Added UserID to interview for admin.

--- src/components/InterviewList.tsx
+++ src/components/InterviewList.tsx
@@ -12,14 +12,23 @@ import Link from "@mui/material/Link";
 export default function InterviewList() {
     const { data: session } = useSession();
     
+    const [userID, setUserID] = useState(null);
     const [loading, setLoading] = useState(true);
     const [refresh, setRefresh] = useState(false);
     const [interviewItems, setInterviewItems] = useState<InterviewItem[]>([]);
+
+    function parseJwt(token: string) {
+        if (!token) { return; }
+        const base64Url = token.split('.')[1];
+        const base64 = base64Url.replace('-', '+').replace('_', '/');
+        return JSON.parse(window.atob(base64));
+    }
 
     useEffect(() => {
         if (session) {
             (async function (){
                 try {
+                    setUserID(parseJwt((session as any).user.token).id)
                     const data = await getInterviews((session as any).user.token);
                     setInterviewItems(data.data)
                 } catch (error) {
@@ -50,6 +59,7 @@ export default function InterviewList() {
            (<>
                 { interviewItems.length > 0 ? interviewItems.map((item:InterviewItem)=> (
                     <div className="bg-slate-200 rounded px-5 py-2 my-2" key={item._id}>
+                        {userID != item.user ? <div className="text-md text-black">User ID: {item.user}</div> : <></>}
                         <div className="text-md text-black">At: {item.company.name}</div>
                         <div className="text-md text-black">On: {dayjs(item.interviewDate).format("YYYY-MM-DD")}</div>
                         <Link className="inline rounded-md bg-teal-400 hover:bg-teal-600 px-3 py-2.5 text-white shadow-sm my-1 mr-1.5 no-underline" href={`/interview/${item._id}`}>
